d24da19a09d32bd9291361ab13be755298b33679
HerbertGM <herbertperugm@gmail.com>
2022-07-30 22:42:20 -0500

    first commit

---
+++ C#/01-Introduccion/Ejercicios_sesion_1/Program.cs
@@ -0,0 +1,14 @@
+﻿// Ejercicio 1
+// Escribe un programa que reciba tu nombre y lo escriba por consola.
+Console.WriteLine("Escribe tu nombre");
+
+string nombre = Console.ReadLine();
+
+Console.WriteLine("Bienvenido: " + nombre);
+
+
+//Ejercicio 2
+//Escribe un programa que tome la hora y la escriba por pantalla.
+
+string datetime = DateTime.Now.ToString("hh:mm:ss tt");
+Console.WriteLine("La hora actual es: " + datetime);
---
+++ C#/01-Introduccion/README.md
@@ -0,0 +1,14 @@
+# OPEN-BOOTCAMP
+
+## Curso de C#
+### Tema 1
+
+- Ejercicio 1 
+
+    Escribe un programa que reciba tu nombre y lo escriba por consola.
+
+    ***
+
+- Ejercicio 2
+    
+    Escribe un programa que tome la hora y la escriba por pantalla.
